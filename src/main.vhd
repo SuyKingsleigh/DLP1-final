46e085b2b91158486794cf507ae8d01cd4b77020
SuyKingsleigh <suymorielwiese@gmail.com>
2019-06-30 22:50:30 -0300

    .

--- src/main.vhd
+++ src/main.vhd
@@ -4,16 +4,21 @@ use IEEE.numeric_std.all;
 
 entity main is
   generic(
-      n : natural := 12;
+      n : natural := 11;
       n_ascii : natural := 7;
-      n_ssd : natural := 7
+      n_ssd : natural := 7;
+      n_msg : natural := 8
   ); -- start, 8 bits msg, paridade, 2 stop; 
   port (
       a, b, c, d, e, f, g : in std_logic; 
+      serial_in : in std_logic;
+      other_clk_in : in std_logic;
       save, main_rst, main_clk, parity_mode : in std_logic;
       sel : in std_logic_vector(1 downto 0);
       tx : out std_logic;
-      parity_mode_out, paridade, baudrate : out std_logic
+      erro_trans : out std_logic;
+      parity_mode_out, paridade, baudrate : out std_logic;
+      outA, outB, outC, outD, outE, outF, outG : out std_logic
   );
 end main;
 
@@ -70,6 +75,21 @@ architecture rtl of main is
     );
   end component;
 
+  component serial2parallel is 
+    port (
+      serial_in, clk : in std_logic; 
+      a,b,c,d,e,f,g, parity_bit : out std_logic    
+    );
+  end component;
+
+  component parity_checker is 
+    port (
+      parity_mode : in std_logic; 
+      data : in std_logic_vector(n_msg -1 downto 0);
+      x : out std_logic
+  );
+  end component;
+
 ------------------------------------------------------------------------------
 ------------- SINAIS ---------------------------------------------------------
 signal saved_sign : std_logic; 
@@ -78,6 +98,7 @@ signal ssd_sign : std_logic_vector(n_ssd - 1 downto 0);
 signal parity_sign : std_logic_vector(n_ascii downto 0); 
 signal baud_rate_sign :  std_logic;
 signal parity_mode_sign : std_logic;
+signal serial2parallel_sign : std_logic_vector(n_msg-1 downto 0);
 
 --------------------------------------------------------------------------------
 begin
@@ -125,11 +146,41 @@ begin
     tx => tx, 
     clk => baud_rate_sign
   );
+
+  
+  serialtoparallel: serial2parallel 
+  port map(
+    serial_in => serial_in, 
+    clk => other_clk_in, 
+    a => serial2parallel_sign(0),
+    b => serial2parallel_sign(1),
+    c => serial2parallel_sign(2),
+    d => serial2parallel_sign(3),
+    e => serial2parallel_sign(4),
+    f => serial2parallel_sign(5),
+    g => serial2parallel_sign(6),
+    parity_bit => serial2parallel_sign(7)
+  );
+
+  parityChecker : parity_checker 
+  port map(
+      parity_mode => parity_mode,
+      data => serial2parallel_sign,
+      x => erro_trans
+  );
  -------------------- CODIGO CONCORRENTE ----------------------------------------------------------
  
  baudrate <= baud_rate_sign; -- da o baudrate gerado
  parity_mode_sign <= parity_mode;
  parity_mode_out <= parity_mode_sign;
- 
+
+  -- mensagem recebida serial->paralelo
+  outA <= serial2parallel_sign(0);
+  outB <= serial2parallel_sign(1);
+  outC <= serial2parallel_sign(2);
+  outD <= serial2parallel_sign(3);
+  outE <= serial2parallel_sign(4);
+  outF <= serial2parallel_sign(5);
+  outG <= serial2parallel_sign(6);
 
 end architecture rtl;
---
+++ src/parity_checker.vhd
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity parity_checker is
+    generic(n : natural := 8);
+    port (
+        parity_mode : in std_logic; 
+        data : in std_logic_vector(n-1 downto 0);
+        x : out std_logic
+    );
+end entity parity_checker;
+
+architecture rtl of parity_checker is
+    signal aux : std_logic_vector(n-1 downto 0);	 
+begin
+    
+    aux(0) <= data(0) xor data(1);
+    xor_loop : for i in 0 to 4 generate
+        aux(i+1) <= aux(i) xor data(i + 2);
+    end generate xor_loop;
+
+    x <= aux(n-2) xnor parity_mode;
+
+end architecture rtl;
